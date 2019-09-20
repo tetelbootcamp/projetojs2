eef30863b53c55e84ff66a007228b18a92ba6493
Marcelo Fernandes <tetel.player@gmail.com>
2019-09-20 10:39:15 -0300

    commit src

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'postgres',
+  username: 'postgres',
+  database: 'dados',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
